f91d441bd1b0415fd2b87f8c1243d8a9d2cbd4b5
Huaqing Xu <huaqingxu89@gmail.com>
2023-05-16 14:15:58 +0800

    u

--- round2/2_rank_feature.py
+++ round2/2_rank_feature.py
@@ -3,5 +3,5 @@ from round2.round2_base import get_sample, get_hdf
 if __name__ == "__main__":
     sample = get_sample(dtype='rank')
     
-    df = get_hdf('all', if_filter_label=True, if_drop_duplicates=True, if_lastday=True)
-    print(df.head())
+    # df = get_hdf('all', if_filter_label=True, if_drop_duplicates=True, if_lastday=True)
+    # print(df.head())
